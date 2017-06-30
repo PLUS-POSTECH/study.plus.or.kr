63fe8dd48de23cacef61bf0cea35988c5562bed4
Kangsu Kim <pica744@gmail.com>
2017-06-30 15:25:55 +0900

    Completed Basic Seminar Page (with some coding error fix)

--- src/website/urls.py
+++ src/website/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^register/$', views.RegisterView.as_view(), name='register')
+    url(r'^register/$', views.RegisterView.as_view(), name='register'),
     url(r'^seminar/$', views.SeminarListView.as_view(), name='seminar')
 ]

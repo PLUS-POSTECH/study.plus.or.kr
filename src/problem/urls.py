d19e7a221482ed6ddec201b31f14255d1c1ae33c
Kangsu Kim <pica744@gmail.com>
2017-07-14 18:06:51 +0900

    Add auth to "problem", routed urls.

--- src/problem/urls.py
+++ src/problem/urls.py
@@ -4,6 +4,7 @@ from problem import views
 
 urlpatterns = [
     url(r'^download$', views.DownloadView.as_view(), name='download_problem'),
-    url(r'^get$', views.DownloadView.as_view(), name='get_problem'),
+    url(r'^auth$', views.ProblemAuthForm.as_view(), name='auth_problem'),
+    url(r'^get$', views.ProblemGetForm.as_view(), name='get_problem'),
     url(r'^', views.ProblemListView.as_view(), name='list_problem')
 ]

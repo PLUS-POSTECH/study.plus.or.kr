85a88ea9cca8cc5ef6453bedc2406d376a014b65
Kangsu Kim <pica744@gmail.com>
2017-07-19 17:51:10 +0900

    Changed "problem" models to manage auth logs. Changed DownloadView to get file's primary key as input form.

--- src/problem/models.py
+++ src/problem/models.py
@@ -1,6 +1,7 @@
 import os
 
 from django.db import models
+from django.core.files.storage import FileSystemStorage
 from django.contrib.auth import get_user_model
 
 from website.models import Category, Session
@@ -9,8 +10,11 @@ from website.models import Category, Session
 User = get_user_model()
 
 
+ProblemAttachmentStorage = FileSystemStorage(location=os.path.join('problem', 'attachments'))
+
+
 class ProblemAttachment(models.Model):
-    file = models.FileField(upload_to='problem' + os.path.sep + 'attachments')
+    file = models.FileField(storage=ProblemAttachmentStorage)
 
     class Meta:
         verbose_name = '문제 첨부파일'
@@ -24,7 +28,7 @@ class ProblemAttachment(models.Model):
 
 
 class Problem(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, unique=True)
     categories = models.ManyToManyField(Category)
     author = models.ForeignKey(User)
     description = models.TextField(blank=True)
@@ -43,31 +47,10 @@ class Problem(models.Model):
         return '%s' % self.title
 
 
-class ProblemInstance(models.Model):
-    problem = models.ForeignKey(Problem)
-    points = models.IntegerField()
-    distributed_points = models.IntegerField()
-    breakthrough_points = models.IntegerField()
-    first_blood = models.CharField(max_length=150, blank=True)
-    solved_users = models.ManyToManyField(User, blank=True)
-    hidden = models.BooleanField()
-
-    class Meta:
-        verbose_name = '문제 인스턴스'
-        verbose_name_plural = '문제 인스턴스들'
-
-    def solved_count(self):
-        return len(self.solved_users.all())
-
-    def __str__(self):
-        return '%s' % self.problem.title
-
-
 class ProblemList(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
     session = models.ForeignKey(Session)
-    problem_instances = models.ManyToManyField(ProblemInstance)
 
     class Meta:
         verbose_name = '문제 리스트'
@@ -75,3 +58,26 @@ class ProblemList(models.Model):
 
     def __str__(self):
         return '%s' % self.title
+
+
+class ProblemInstance(models.Model):
+    problem = models.ForeignKey(Problem)
+    problem_list = models.ForeignKey(ProblemList)
+    points = models.IntegerField()
+    distributed_points = models.IntegerField()
+    breakthrough_points = models.IntegerField()
+    hidden = models.BooleanField()
+
+    class Meta:
+        verbose_name = '문제 인스턴스'
+        verbose_name_plural = '문제 인스턴스들'
+
+    def __str__(self):
+        return '%s' % self.problem.title
+
+
+class ProblemAuthLog(models.Model):
+    user = models.ForeignKey(User)
+    problem_instance = models.ForeignKey(ProblemInstance)
+    auth_key = models.TextField()
+    datetime = models.DateTimeField()

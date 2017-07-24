cbd96427efa39b209319772de67da868c3f4bc4d
Kangsu Kim <pica744@gmail.com>
2017-07-24 21:35:26 +0900

    Fixed URL Pattern Now uses url template rather than hardcoding.

--- src/problem/urls.py
+++ src/problem/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from problem import views
 
 urlpatterns = [
-    url(r'^download$', views.DownloadView.as_view(), name='download_problem'),
-    url(r'^auth$', views.ProblemAuthView.as_view(), name='auth_problem'),
-    url(r'^get$', views.ProblemGetView.as_view(), name='get_problem'),
-    url(r'^', views.ProblemListView.as_view(), name='list_problem')
+    url(r'^download/(?P<pk>\d+)$', views.DownloadView.as_view(), name='download'),
+    url(r'^auth/(?P<pk>\d+)$', views.ProblemAuthView.as_view(), name='auth'),
+    url(r'^get/(?P<pk>\d+)$', views.ProblemGetView.as_view(), name='get'),
+    url(r'^$', views.ProblemListView.as_view(), name='list')
 ]

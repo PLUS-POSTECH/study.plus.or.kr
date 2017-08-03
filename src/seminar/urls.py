4ad0ceeb8fc4e69d21587a7d4ed99eeec6d6da94
Kangsu Kim <pica744@gmail.com>
2017-08-04 02:01:52 +0900

    Download view renamed for integrity.

--- src/seminar/urls.py
+++ src/seminar/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from seminar import views
 
 urlpatterns = [
-    url(r'^download/(?P<pk>\d+)$', views.DownloadView.as_view(), name='download'),
+    url(r'^download/(?P<pk>\d+)$', views.SeminarDownloadView.as_view(), name='download'),
     url(r'^$', views.SeminarListView.as_view(), name='list')
 ]

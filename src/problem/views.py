ce3e169b7dab506574120b9aac69a67ec165476d
imnotkind <billy307@daum.net>
2018-07-11 00:09:42 +0900

    datetime descending order

--- src/problem/views.py
+++ src/problem/views.py
@@ -288,8 +288,8 @@ class ProblemRankView(PlusMemberCheck, View):
 
 class ProblemQuestionView(PlusMemberCheck, View):
     def get(self, request):
-        questions = ProblemQuestion.objects.filter(user=request.user).order_by('datetime') 
-        answers = ProblemQuestion.objects.order_by('datetime') 
+        questions = ProblemQuestion.objects.filter(user=request.user).order_by('-datetime') 
+        answers = ProblemQuestion.objects.order_by('-datetime') 
         answers = list(filter(lambda x: x.problem_instance.problem.author == request.user,answers)) 
          
         return render(request, 'problem/question.html', { 

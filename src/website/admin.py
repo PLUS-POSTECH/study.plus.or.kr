b0993ae369e39c4ac9a507db20df09cc1cf02a99
Unknown <ewqqw@naver.com>
2018-07-22 00:37:23 +0900

    Setting for CI check

--- src/website/admin.py
+++ src/website/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import User, Category, Session, Notification
 
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email')
@@ -14,5 +15,6 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Session)
 class SessionAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
+
 
 admin.site.register(Notification)

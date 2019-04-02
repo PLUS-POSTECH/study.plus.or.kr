dfee03e29b827aaaf791a2d8b218527e09ef29aa
imnotkind <imnotkind307@gmail.com>
2019-04-02 23:20:41 +0900

    dunno

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)$', views.ShopProdView.as_view(), name='prod'),
     url(r'^$', views.ShopProdView.as_view(), name='prod'),
     url(r'^inven/$', views.ShopInvenView.as_view(), name='inven'),
-    url(r'^/retrieve/(?P<pk>\d+)$', views.ShopRetrieveView.as_view(), name='retrieve')
+    url(r'^retrieve/(?P<pk>\d+)$', views.ShopRetrieveView.as_view(), name='retrieve')
 ]

bb6d72bfe45dea899f59f6919c6830cec5d91bea
imnotkind <imnotkind307@gmail.com>
2019-04-02 16:08:25 +0900

    minor fix

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -5,9 +5,9 @@ from shop import views
 app_name = 'shop'
 
 urlpatterns = [
-    url(r'^prod/(?P<pk>\d+)/buy$', views.ShopPurchaseView.as_view(), name='buy'),
-    url(r'^prod/(?P<pk>\d+)$', views.ShopProdView.as_view(), name='prod'),
-    url(r'^prod/$', views.ShopProdView.as_view(), name='prod'),
+    url(r'^/(?P<pk>\d+)/buy$', views.ShopPurchaseView.as_view(), name='buy'),
+    url(r'^/(?P<pk>\d+)$', views.ShopProdView.as_view(), name='prod'),
+    url(r'^/$', views.ShopProdView.as_view(), name='prod'),
     url(r'^inven/$', views.ShopInvenView.as_view(), name='inven')
 
 ]

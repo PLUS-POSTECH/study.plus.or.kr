cb1607b2addf9a7544701b58115bda081c8ef0ca
imnotkind <imnotkind307@gmail.com>
2019-04-02 15:59:07 +0900

    show shop in admin

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ShopItem, Shop, ShopPurchaseLog
+
+
+@admin.register(ShopItem)
+class ShopItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'hidden')
+    readonly_fields = ('last_modified', )
+
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('title', 'problem_list', 'description')
+    readonly_fields = ('last_modified', )
+
+
+@admin.register(ShopPurchaseLog)
+class ShopPurchaseLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'shop', 'item', 'succeed', 'retrieved')
+    readonly_fields = ('purchase_time', )

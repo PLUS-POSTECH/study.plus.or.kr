dcecae5ebe5353a3eac46a2c0d9df22f60dacbe5
imnotkind <imnotkind307@gmail.com>
2019-04-26 20:29:27 +0900

    ci fix

--- src/problem/views.py
+++ src/problem/views.py
@@ -228,13 +228,13 @@ class ProblemUserView(PlusMemberCheck, View):
         for problem_list in problem_lists:
             total = get_problem_list_total_score(problem_list)
             problem_lists_with_total.append({"problem_list": problem_list, "total": total})
-        
+
         solved_log_queries = []
         for problem_instance in ProblemInstance.objects.all():
             correct_auth_key = problem_instance.problem.auth_key
             solve_logs = ProblemAuthLog.objects.filter(problem_instance=problem_instance, auth_key=correct_auth_key)
             solved_log_queries.append(solve_logs)
-        
+
         solve_logs = \
             reduce(lambda x, y: x | y, solved_log_queries, ProblemAuthLog.objects.none()) \
             .order_by('datetime')

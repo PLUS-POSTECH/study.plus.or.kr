ca23014abbb29a092b0e0e8a10051eddb76e803b
imnotkind <imnotkind307@gmail.com>
2021-03-22 18:33:05 +0900

    ci fix

--- src/plus_study/settings.py
+++ src/plus_study/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'USER': 'postgres',
         'HOST': 'db',
         'PORT': 5432,
-        #'PASSWORD': (Replace with your db password)
+        # 'PASSWORD': (Replace with your db password)
     }
 }
 

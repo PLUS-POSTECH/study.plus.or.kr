0c10e99d10c9c946ee94e653d6fcd701f66706a6
beta-lux <beta.underalpha@gmail.com>
2021-03-23 12:16:31 +0900

    signal for discord integrity

---
+++ src/problem/signals.py
@@ -0,0 +1,18 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import ProblemInstance, ProblemQuestion
+from integration.helpers import discord
+
+
+@receiver(post_save, sender=ProblemInstance)
+def on_register_problem_handler(sender, instance, **kwargs):
+    discord.on_problem_registered(instance)
+
+
+@receiver(post_save, sender=ProblemQuestion)
+def on_question_handler(sender, instance: ProblemQuestion, **kwargs):
+    if instance.answer == "":
+        discord.on_question(instance)
+    else:
+        discord.on_answer(instance)

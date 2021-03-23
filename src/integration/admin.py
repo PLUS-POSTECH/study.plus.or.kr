eef3b5c461904a683804b67868fd6b0c2143d21c
beta-lux <beta.underalpha@gmail.com>
2021-03-23 11:47:51 +0900

    admin fieldset

--- src/integration/admin.py
+++ src/integration/admin.py
@@ -5,4 +5,23 @@ from .models import Discord
 
 @admin.register(Discord)
 class DiscordAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = [
+        (None, {
+            'fields': ['title', 'url_webhook', 'url_avatar', 'is_active']
+        }),
+        ('On First Blood', {
+            'fields': ['on_first_blood', 'color_first_blood']
+        }),
+        ('On Solved', {
+            'fields': ['on_solved', 'color_solved']
+        }),
+        ('On Problem Registered', {
+            'fields': ['on_problem_registered', 'color_on_problem_registered']
+        }),
+        ('On Question', {
+            'fields': ['on_question', 'color_on_question']
+        }),
+        ('On Answer', {
+            'fields': ['on_answer', 'color_on_answer']
+        })
+    ]

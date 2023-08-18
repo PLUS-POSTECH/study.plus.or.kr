1f56407e5c0293aa111006dae786b3186b1fd245
Minjae Gwon <beta.underalpha@gmail.com>
2023-08-18 17:58:42 +0900

    api: Init core models

--- packages/api/src/server/core/models.py
+++ packages/api/src/server/core/models.py
@@ -1,3 +1,55 @@
+from typing import TYPE_CHECKING
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+if TYPE_CHECKING:
+    MetaProtocol = models.Model.Meta
+else:
+    MetaProtocol = object
+
+
+class User(AbstractUser):
+    povis_id = models.CharField(max_length=20, blank=True)
+    is_plus_member = models.BooleanField(default=False)
+
+    class Meta(MetaProtocol):
+        verbose_name = "사용자"
+        verbose_name_plural = "사용자들"
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    description = models.TextField(blank=True)
+
+    class Meta(MetaProtocol):
+        verbose_name = "카테고리"
+        verbose_name_plural = "카테고리들"
+
+
+class Session(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=False)
+
+    class Meta(MetaProtocol):
+        verbose_name = "분기"
+        verbose_name_plural = "분기들"
+
+    def __str__(self):
+        return f"<{self.__class__.__name__}: {self.title}>"
+
+
+class Notification(MetaProtocol):
+    title = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=False)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta(MetaProtocol):
+        verbose_name = "공지사항"
+        verbose_name_plural = "공지사항들"
+
+    def __str__(self):
+        return f"<{self.__class__.__name__}: {self.title}>"
--- packages/api/src/server/study/settings.py
+++ packages/api/src/server/study/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "core.apps.CoreConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -121,3 +122,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "core.User"
